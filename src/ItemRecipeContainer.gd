c8c025cec558ac2971d35f10ab173029b51fe444
newshamu <johnsonc.2423@gmail.com>
2021-07-07 23:28:15 -0400

    Incorporated resized items

--- src/ItemRecipeContainer.gd
+++ src/ItemRecipeContainer.gd
@@ -3,8 +3,21 @@ extends PanelContainer
 class_name ItemRecipeContainer
 
 onready var container: VBoxContainer = $ScrollContainer/VBoxContainer
+onready var empty_label: Label
 
 var ItemRecipePanelScene: PackedScene = preload("res://src/UI/ItemRecipePanel.tscn")
+
+
+func _ready():
+	var overlay: CanvasLayer = get_tree().root.get_node("App").get_node("Overlay")
+	empty_label = overlay.get_node("EmptyLabel")
+
+
+func _process(_delta):
+	if container.get_children().size() > 0:
+		empty_label.visible = false
+	else:
+		empty_label.visible = true
 
 
 func add_item(item: Item):
--- src/UI/FinishedItemButton.gd
+++ src/UI/FinishedItemButton.gd
@@ -9,15 +9,5 @@ signal finished_item_selected(item)
 
 func _on_FinishedItemButton_pressed():
 	print(item.item_name + " clicked")
+	print("Icon size: " + str(texture_normal.get_size().x) + "x" + str(texture_normal.get_size().y))
 	emit_signal("finished_item_selected", item)
-
-
-func scale_image():
-	print(texture_normal.get_size())
-	if texture_normal.get_size() == Vector2(128, 128):
-		var image: Image = texture_normal.get_data()
-		image.resize(64, 64)
-		print(image)
-		var itex: ImageTexture = ImageTexture.new()
-		itex.create_from_image(image)
-		texture_normal = itex
